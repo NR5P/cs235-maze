0ececa6aa6f64460f3830efebd4dafcd3045c092
NR5P <orangev8zcar@gmail.com>
2019-07-18 20:18:39 -0500

    list added along with methods declared for graph

--- graph.h
+++ graph.h
@@ -1,11 +1,33 @@
 #ifndef GRAPH_H
 #define GRPAH_H
 
-class Graph {
-    private:
+#include "vertex.h"
+#include "bst.h"
+#include "set.h"
+#include "list.h"
 
+class Graph {
+    private:    
+        int numVertices;
 
     public:
+        // non default constructor
+        Graph(int numVertices) : numVertices(numVertices){}
+        
+        // assignment 
+        Graph &operator=(const Graph &rhs);
+
+        bool empty();
+        int size();
+        int capacity();
+        void clear();
+        void add(Vertex v1, Vertex v2);
+        bool isEdge(Vertex v1, Vertex v2);
+        custom::set<Vertex> findEdges(Vertex v);
+        custom::list<Vertex> findPath(Vertex v1, Vertex v2);
+
+
+
 };
 
 #endif
